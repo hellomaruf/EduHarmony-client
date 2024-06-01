ac876ee70cfea54c852b299a96a825cb9decf0a5
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-01 11:11:50 +0600

    Added some update

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import google from "../assets/Images/google.png";
 function SignUp() {
   return (
     <div>
@@ -11,108 +12,119 @@ function SignUp() {
             nulla eaque error neque ipsa culpa autem, at itaque nostrum!
           </p>
         </div>
+        <div className="max-w-md mx-auto">
+          <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
+            <div>
+              <label className="sr-only">Name</label>
 
-        <form action="#" className="mx-auto mb-0 mt-8 max-w-md space-y-4">
-          <div>
-            <label className="sr-only">Name</label>
-
-            <div className="relative">
-              <input
-                type="text"
-                name="name"
-                className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter Name"
-              />
-
-             
+              <div className="relative">
+                <input
+                  type="text"
+                  name="name"
+                  className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
+                  placeholder="Enter Name"
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <label htmlFor="email" className="sr-only">
-              Email
-            </label>
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
 
-            <div className="relative">
-              <input
-                type="email"
-                name="email"
-                className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter email"
-              />
+              <div className="relative">
+                <input
+                  type="email"
+                  name="email"
+                  className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
+                  placeholder="Enter email"
+                />
 
-              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+
+              <div className="relative">
+                <input
+                  type="password"
+                  name="password"
+                  className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
+                  placeholder="Enter password"
+                />
+
+                <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="size-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                    />
+                  </svg>
+                </span>
+              </div>
+            </div>
+            <input
+              type="file"
+              className="file-input file-input-ghost bg-gray-100 w-full rounded-full"
+            />
+
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-500">
+                No account?
+                <Link
+                  to="/signIn"
+                  className="underline text-[#7330FF]"
+                  href="#"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-                  />
-                </svg>
-              </span>
+                  Sign In
+                </Link>
+              </p>
             </div>
-          </div>
 
-          <div>
-            <label htmlFor="password" className="sr-only">
-              Password
-            </label>
-
-            <div className="relative">
-              <input
-                type="password"
-                name="password"
-                className="w-full rounded-lg border-gray-200 bg-slate-50 p-4 pe-12 text-sm shadow-sm"
-                placeholder="Enter password"
-              />
-
-              <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="size-4 text-gray-400"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                  />
-                </svg>
-              </span>
-            </div>
-          </div>
-          <input type="file" className="file-input file-input-ghost bg-gray-100 w-full rounded-full" />
-
-          <div className="flex items-center justify-between">
-            <p className="text-sm text-gray-500">
-              No account?
-              <Link to="/signIn" className="underline text-[#7330FF]" href="#">
-                Sign In
-              </Link>
-            </p>
-          </div>
-          <button
-            type="submit"
-            className="inline-block w-full  bg-[#7330FF] rounded-full px-5 py-3 text-sm font-medium text-white"
-          >
-            Sign Up
+            <button
+              type="submit"
+              className="inline-block w-full  bg-[#7330FF] rounded-full px-5 py-3 text-sm font-medium text-white"
+            >
+              Sign Up
+            </button>
+          </form>
+          <button className="btn w-full mt-4 rounded-full">
+            Sign Up with Google
+           <img src={google} className="w-6" alt=""  />
           </button>
-        </form>
+        </div>
       </div>
     </div>
   );
