2eb5bfa14b550b23f275e98a5626f472de8237bf
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-02 09:23:35 +0600

    Added teach on edu form

--- src/Pages/TeachOnEdu.jsx
+++ src/Pages/TeachOnEdu.jsx
@@ -1,10 +1,106 @@
+import { useContext } from "react";
+import { AuthContext } from "../Services/AuthProvider";
 
 function TeachOnEdu() {
+  const { user } = useContext(AuthContext);
+  const handleApplyTeaching = () => {};
   return (
-    <div>
-      Teach on edu
+    <div className="max-w-7xl mx-auto">
+      <form
+        onSubmit={handleApplyTeaching}
+        noValidate=""
+        action=""
+        className="container flex flex-col mx-auto space-y-12"
+      >
+        <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-2xl shadow-sm  border-2 bg-base-100 border-[#7330FF]">
+          <div className="grid grid-cols-6 gap-4 col-span-full ">
+            <div className="col-span-full sm:col-span-3">
+              <label htmlFor="lastname" className="text-lg font-medium">
+                Name
+              </label>
+              <input
+                required
+                name="bookName"
+                type="text"
+                placeholder="Enter your Name"
+                className="w-full border focus:bg-base-200 focus:border-[#7330FF]  rounded-md p-2 outline-none  "
+              />
+            </div>
+            <div className="col-span-full sm:col-span-3">
+              <label htmlFor="lastname" className="text-lg font-medium">
+                Email
+              </label>
+              <input
+                required
+                defaultValue={user?.email}
+                disabled
+                name="bookName"
+                type="text"
+                placeholder="Enter Book Name"
+                className="w-full border focus:bg-base-200 focus:border-[#7330FF]  rounded-md p-2 outline-none  "
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-6 gap-4 col-span-full ">
+            <div className="col-span-full sm:col-span-3">
+              <label htmlFor="firstname" className="text-lg font-medium">
+                Experience
+              </label>
+              <select
+                className="w-full rounded-md focus:bg-base-200  p-2 outline-none border"
+                name="cateName"
+                id="cars"
+              >
+                <option value="beginner">Beginner</option>
+                <option value="experienced">Experienced</option>
+                <option value="mid-level">Mid-level</option>
+              </select>
+            </div>
+            <div className="col-span-full sm:col-span-3">
+              <label htmlFor="lastname" className="text-lg font-medium">
+                Title
+              </label>
+              <input
+                required
+                name="title"
+                type="text"
+                placeholder="Enter a Title"
+                className="w-full focus:bg-base-200 focus:border-[#7330FF] rounded-md p-2 outline-none border "
+              />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-6 gap-4 col-span-full ">
+            <div className="col-span-full ">
+              <label htmlFor="firstname" className="text-lg font-medium">
+                Category
+              </label>
+              <select
+                className="w-full rounded-md focus:bg-base-200  p-2 outline-none border"
+                name="cateName"
+                id="cars"
+              >
+                <option value="beginner">Web Development</option>
+                <option value="experienced">Mobile App Development</option>
+                <option value="mid-level">Digital Marketing</option>
+                <option value="mid-level">Cybersecurity</option>
+                <option value="mid-level">Cloud Computing</option>
+                <option value="mid-level">DSA</option>
+              </select>
+            </div>
+
+            <div className="">
+              <input
+                className="bg-[#7330FF] hover:bg-[#8851ff] btn text-white   py-2 font-bold  rounded-lg"
+                type="submit"
+                value="Apply for Teaching"
+              />
+            </div>
+          </div>
+        </fieldset>
+      </form>
     </div>
-  )
+  );
 }
 
-export default TeachOnEdu
+export default TeachOnEdu;
