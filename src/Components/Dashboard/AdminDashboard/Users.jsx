09fd15828cb3e3b4a26c8957f12fc39a71700dae
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-02 13:45:43 +0600

    Added make admin functionality

--- src/Components/Dashboard/AdminDashboard/Users.jsx
+++ src/Components/Dashboard/AdminDashboard/Users.jsx
@@ -2,10 +2,15 @@ import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { MdOutlineAdminPanelSettings } from "react-icons/md";
 import Spinner from "../../../Utils/Spinner";
+import Swal from "sweetalert2";
 
 function Users() {
   const axiosSecure = useAxiosSecure();
-  const { data: users, isLoading } = useQuery({
+  const {
+    data: users,
+    isLoading,
+    refetch,
+  } = useQuery({
     queryKey: "users",
     queryFn: async () => {
       const { data } = await axiosSecure.get("/users");
@@ -13,6 +18,22 @@ function Users() {
     },
   });
   console.log(users);
+  const handleMakeAdmin = async (item) => {
+    await axiosSecure.patch(`/users/${item?._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        Swal.fire({
+          title: `${item?.name} is now Admin!`,
+          text: "Do you want to continue",
+          icon: "success",
+          confirmButtonColor: "#7330ff",
+          confirmButtonText: "Cool",
+        });
+        refetch();
+      }
+    });
+  };
+  console.log(users?.role);
   return (
     <div>
       <div className="overflow-x-auto">
@@ -54,10 +75,17 @@ function Users() {
                       {item?.email}
                     </span>
                   </td>
-                  <th className="flex ">
-                    <button className=" text-2xl btn-circle flex items-center justify-center bg-orange-50 hover:bg-orange-100 rounded-full text-orange-500">
-                      <MdOutlineAdminPanelSettings />
-                    </button>
+                  <th className="flex font-normal">
+                    {item?.role === "admin" ? (
+                      "Admin"
+                    ) : (
+                      <button
+                        onClick={() => handleMakeAdmin(item)}
+                        className=" text-2xl btn-circle flex items-center justify-center bg-orange-50 hover:bg-orange-100 rounded-full text-orange-500"
+                      >
+                        <MdOutlineAdminPanelSettings />
+                      </button>
+                    )}
                   </th>
                 </tr>
               ))}
