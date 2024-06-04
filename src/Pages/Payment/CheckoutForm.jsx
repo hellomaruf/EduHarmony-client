677dbaf6d7e4d01b8bedaecff6fbd676bb36f278
maruf-ahmed <marufahamed1110@gmail.com>
2024-06-04 12:12:51 +0600

    Added payment functionality successfully

--- src/Pages/Payment/CheckoutForm.jsx
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -1,11 +1,15 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useAxiosSecure from "../../Hooks/useAxiosSecure";
 import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../Services/AuthProvider";
+import { useMutation } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 function CheckoutForm() {
+  const { user } = useContext(AuthContext);
   const data = useLoaderData();
-  console.log(data?.price);
+  console.log(data);
   const axiosSecure = useAxiosSecure();
   const stripe = useStripe();
   const elements = useElements();
@@ -19,12 +23,28 @@ function CheckoutForm() {
         setClientSecret(res.data.clientSecret);
       });
   }, []);
+  const mutation = useMutation({
+    mutationFn: (NewClass) => {
+      return axiosSecure.post("/payment", NewClass);
+    },
+    onSuccess: (data) => {
+      // Boom baby!
+      console.log(data);
+      Swal.fire({
+        icon: "success",
+        title: "Payment Successfull!!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    },
+  });
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
     const card = elements.getElement(CardElement);
+
     if (card == null) {
       return;
     }
@@ -41,7 +61,37 @@ function CheckoutForm() {
       console.log("[PaymentMethod]", paymentMethod);
       setError("");
     }
+
+    // Comfirm payment
+    const { paymentIntent, error: confirmError } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email || "anonymous",
+            name: user?.displayName || "anonymous",
+          },
+        },
+      });
+    console.log(paymentIntent);
+    if (confirmError) {
+      console.log("Confirm Error");
+    } else {
+      console.log("payment intent", paymentIntent);
+    }
+
+    // Save payment info in database
+    const paymentInfo = {
+      email: user?.email,
+      transactionId: paymentIntent?.id,
+      price: data?.price,
+      classId: data?._id,
+      date: new Date(),
+      status: "pending",
+    };
+    mutation.mutate(paymentInfo);
   };
+
   return (
     <div className="max-w-2xl mx-auto">
       <form onSubmit={handleSubmit}>
@@ -66,7 +116,7 @@ function CheckoutForm() {
           type="submit"
           disabled={!stripe || !clientSecret}
         >
-          Pay
+          Pay ${data?.price}
         </button>
         <p className="text-red-500 text-sm">{error}</p>
       </form>
